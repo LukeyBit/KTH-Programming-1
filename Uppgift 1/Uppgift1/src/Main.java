849481aae4a8a317f1dc462bef9d856aa3f86de7
Lukas <lukas@lukasandersson.com>
2023-11-02 13:01:11 +0100

    Created Uppgift1
    Added assignment files

---
+++ Uppgift 1/Uppgift1/src/Main.java
@@ -0,0 +1,18 @@
+// Author Lukas Andersson
+// CINTE
+// KTH
+
+/*
+    * Main class
+    * @version 1.0
+ */
+public class Main {
+
+    /**
+     * Main method
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
